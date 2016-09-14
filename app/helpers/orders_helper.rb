e615341452a07ac060414afb97370ce9af9fe82a
Pedro Henrique <pedrohenriqueacruz@gmail.com>
2016-09-14 03:31:46 -0300

    added total price helper

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,3 +1,10 @@
 # -*- encoding : utf-8 -*-
 module OrdersHelper
+  def total_price(order)
+    order.unit_price * order.quantity
+  end
+
+  def total_receipt(orders)
+    orders.map {|order| total_price(order) }.inject(:+)
+  end
 end
