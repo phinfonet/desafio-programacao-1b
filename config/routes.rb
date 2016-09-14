9189fb1b3fa02324b5d13546c7a3d6ee02908a01
Pedro Henrique <pedrohenriqueacruz@gmail.com>
2016-09-14 03:16:23 -0300

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'orders#index'
   get '/orders', to: 'orders#index'
   get '/orders/upload', to: 'orders#upload'
-  get '/orders/:id', to: 'orders#show'
   post '/orders/store_from_file', to: 'orders#store_from_file'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
